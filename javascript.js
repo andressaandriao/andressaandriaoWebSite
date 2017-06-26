2eaf5cc3a27d52ebce7523dddd765f85a499ef6d
Andressa Andrião <aandriao@The-Doctor.local>
2017-06-26 14:52:47 -0300

    New files: javascript file, memorias da lua interactive page, about page

---
+++ javascript.js
@@ -0,0 +1,16 @@
+function main (){
+	$('.livro').mouseover(function(){
+		$(this).fadeTo('fast', 0.5);
+	});
+	$('.livro').mouseleave(function(){
+		$(this).fadeTo(50, 1);
+	});
+	$('#menuBar ul li').mouseover(function (){
+		$(this).fadeTo('fast', 0.5);
+	});
+	$('#menuBar ul li').mouseleave(function (){
+		$(this).fadeTo(10, 1);
+	});
+}
+
+$(document).ready(main);
